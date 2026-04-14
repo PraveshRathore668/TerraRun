77c99086994f2a046ba1e708bc2147a9da3048ae
PraveshRathore668 <praveshrathore2027@gmail.com>
2026-04-15 01:00:45 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,13 +6,17 @@ from app.routes.run_routes import router as run_router
 from app.routes.stats_routes import router as stats_router
 from app.routes.leaderboard_routes import router as leaderboard_router
 
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="TerraRun API")
+app.include_router(auth_router)
+app.include_router(run_router)
+app.include_router(leaderboard_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origin_regex=r"https?://.*",
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -21,7 +25,7 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(run_router)
 app.include_router(stats_router)
-app.include_router(leaderboard_router)
+
 
 @app.get("/")
 def root():
