798dcd1afe990184ed38e5914291dbf6484f188c
PraveshRathore668 <praveshrathore2027@gmail.com>
2026-04-15 00:40:31 +0530

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = "postgresql:///terrarun"
+DATABASE_URL = "postgresql://terrarun_user:5NUing8mHoPZrMozMz8AoDpiL6yu8CsW@dpg-d7f8t54vikkc739mib1g-a/terrarun"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
