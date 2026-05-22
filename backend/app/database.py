4cec82dd50daf0cef0f6b3f3dd2f40ce73770fa4
Prvaesh Singh <praveshrathore2027@gmail.com>
2026-05-22 21:22:27 +0530

    fix: update database config

--- backend/app/database.py
+++ backend/app/database.py
@@ -3,7 +3,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://neondb_owner:npg_2hVzRi9waltF@ep-sweet-boat-ap38qtfg-pooler.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:Pravesh12312@db.jnfjvtcfojduofkrpoec.supabase.co:5432/postgres")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
